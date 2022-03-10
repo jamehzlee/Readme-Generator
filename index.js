5de66ececcca2251b93836c5bf4fa787981e5b69
Jamehzz <probableeme@gmail.com>
2022-03-09 21:30:56 -0500

    added basic prompts and readme template

--- index.js
+++ index.js
@@ -1,10 +1,81 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ({link, repo, description, install, usage, contribution, test}) =>
+    `<h1><a href="${link}"></a>${repo}</h1>
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+## Description
+${description}
+
+## Installation
+${install}
+
+## Usage
+${usage}
+
+## Contributing
+${contribution}
+
+## Tests
+${test}`;
+
+inquirer
+    .prompt(
+        {
+        type: 'input',
+        message: 'Add your deployment link:',
+        name: 'link'
+
+    },
+    {
+        type: 'input',
+        message: 'What is the name of your repo?',
+        name: 'repo'
+
+    },
+    {
+        type: 'input',
+        message: 'Add a README description:',
+        name: 'description'
+
+    },
+    {
+        type: 'input',
+        message: 'Enter installation instructions',
+        name: 'install'
+
+    },
+    {
+        type: 'input',
+        message: 'Enter usage information',
+        name: 'usage'
+
+    },
+    {
+        type: 'input',
+        message: 'Enter contribution guidelines',
+        name: 'contribution'
+
+    },
+    {
+        type: 'input',
+        message: 'Enter test instructions',
+        name: 'test'
+
+    }
+    )
+    .then((answers) => {
+        const data = questions(answers);
+
+        fs.writeFile('README.md', data, (err) =>
+            err ?  console.error(err) : console.log("README created!")
+        )
+    })
+
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
 function init() {}
