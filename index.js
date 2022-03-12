48d9008d7ba776662cc90199af0212327bc5683d
Jamehzz <probableeme@gmail.com>
2022-03-11 22:35:10 -0500

    added license badges/links

--- index.js
+++ index.js
@@ -25,49 +25,48 @@ inquirer
         type: 'input',
         message: 'Add your deployment link:',
         name: 'link'
-
     },
     {
         type: 'input',
         message: 'What is the name of your repo?',
         name: 'repo'
-
     },
     {
         type: 'input',
         message: 'Add a README description:',
         name: 'description'
-
     },
     {
         type: 'input',
         message: 'Enter installation instructions',
         name: 'install'
-
     },
     {
         type: 'input',
         message: 'Enter usage information',
         name: 'usage'
-
     },
     {
         type: 'input',
         message: 'Enter contribution guidelines',
         name: 'contribution'
-
     },
     {
         type: 'input',
         message: 'Enter test instructions',
         name: 'test'
-
+    },
+    {
+        type: 'list',
+        message: 'Choose  a license',
+        choices: ['MIT', 'Apache', 'GNU GPL', 'Mozilla'],
+        name: 'contribution'
     }
     ])
     .then((answers) => {
         const data = questions(answers);
 
-        fs.writeFile('README.md', data, (err) =>
+        fs.writeFile('./dist/README.md', data, (err) =>
             err ?  console.error(err) : console.log("README created!")
         )
     })
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,48 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'MIT') {
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+  }
+  else if (license === 'Apache') {
+    return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache_2.0)`;
+  }
+  else if (license === 'GNU GPL') {
+    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://opensource.org/licenses/gpl-3.0)`;
+  }
+  else if (license === 'Mozilla') {
+    return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;
+  }
+  else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'MIT') {
+    return `https://opensource.org/licenses/MIT`;
+  }
+  else if (license === 'Apache') {
+    return `https://opensource.org/licenses/Apache_2.0`;
+  }
+  else if (license === 'GNU GPL') {
+    return `https://opensource.org/licenses/gpl-3.0`;
+  }
+  else if (license === 'Mozilla') {
+    return `https://opensource.org/licenses/MPL-2.0`;
+  }
+  else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
