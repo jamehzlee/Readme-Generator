9c4b4f112df9221157ee97109151064e8fa73975
Jamehzz <probableeme@gmail.com>
2022-03-12 14:06:17 -0500

    added function to generate license badge

--- index.js
+++ index.js
@@ -1,72 +1,55 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-
-const questions = ({link, repo, description, install, usage, contribution, test}) =>
-`<h1><a href="${link}"></a>${repo}</h1>
-
-## Description
-${description}
-
-## Installation
-${install}
-
-## Usage
-${usage}
-
-## Contributing
-${contribution}
-
-## Tests
-${test}`;
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 inquirer
     .prompt([
-    {
-        type: 'input',
-        message: 'Add your deployment link:',
-        name: 'link'
-    },
-    {
-        type: 'input',
-        message: 'What is the name of your repo?',
-        name: 'repo'
-    },
-    {
-        type: 'input',
-        message: 'Add a README description:',
-        name: 'description'
-    },
-    {
-        type: 'input',
-        message: 'Enter installation instructions',
-        name: 'install'
-    },
-    {
-        type: 'input',
-        message: 'Enter usage information',
-        name: 'usage'
-    },
-    {
-        type: 'input',
-        message: 'Enter contribution guidelines',
-        name: 'contribution'
-    },
-    {
-        type: 'input',
-        message: 'Enter test instructions',
-        name: 'test'
-    },
-    {
-        type: 'list',
-        message: 'Choose  a license',
-        choices: ['MIT', 'Apache', 'GNU GPL', 'Mozilla'],
-        name: 'contribution'
-    }
-    ])
-    .then((answers) => {
-        const data = questions(answers);
+        {
+            type: 'input',
+            message: 'Add your deployment link:',
+            name: 'link'
+        },
+        {
+            type: 'input',
+            message: 'What is the name of your repo?',
+            name: 'repo'
+        },
+        {
+            type: 'input',
+            message: 'Add a README description:',
+            name: 'description'
+        },
+        {
+            type: 'input',
+            message: 'Enter installation instructions:',
+            name: 'install'
+        },
+        {
+            type: 'input',
+            message: 'Enter usage information:',
+            name: 'usage'
+        },
+        {
+            type: 'input',
+            message: 'Enter contributors(If none press enter):',
+            name: 'contribution'
+        },
+        {
+            type: 'input',
+            message: 'Enter test instructions:',
+            name: 'test'
+        },
+        {
+            type: 'list',
+            message: 'Choose  a license',
+            name: 'license',
+            choices: ['MIT', 'Apache', 'GNU GPL', 'Mozilla']
+        }
+        ])
+        .then((data) => {
+            data = generateMarkdown(data);
 
-        fs.writeFile('./dist/README.md', data, (err) =>
-            err ?  console.error(err) : console.log("README created!")
-        )
-    })
+            fs.writeFile('./dist/README.md', data, (err) =>
+                err ?  console.error(err) : console.log("README created!")
+            )
+        })
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,3 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license === 'MIT') {
     return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
@@ -18,37 +16,35 @@ function renderLicenseBadge(license) {
   }
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {
-  if (license === 'MIT') {
-    return `https://opensource.org/licenses/MIT`;
-  }
-  else if (license === 'Apache') {
-    return `https://opensource.org/licenses/Apache_2.0`;
-  }
-  else if (license === 'GNU GPL') {
-    return `https://opensource.org/licenses/gpl-3.0`;
-  }
-  else if (license === 'Mozilla') {
-    return `https://opensource.org/licenses/MPL-2.0`;
+function renderLicenseSection(license) {
+  if (license === 'MIT' ) {
+    return `## License
+${renderLicenseBadge(license)}`;
   }
   else {
     return '';
   }
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
-
-}
-
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `<h1><a href="${data.link}"></a>${data.repo}</h1>
 
-`;
+## Description
+${data.description}
+
+## Installation
+${data.install}
+
+## Usage
+${data.usage}
+
+## Contributors
+${data.contribution}
+
+## Tests
+${data.test}
+
+${renderLicenseSection(data.license)}`;
 }
 
 module.exports = generateMarkdown;
